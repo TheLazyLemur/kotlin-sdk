8c0e0827d3e32b1fc9fe71152915e47e323740a3
Dan Rousseau <danrousseau@protonmail.com>
2022-09-13 12:10:29 +0200

    feat: added create user via email

--- src/main/kotlin/pocketbase/kotlin/services/UserService.kt
+++ src/main/kotlin/pocketbase/kotlin/services/UserService.kt
@@ -16,6 +16,60 @@ import java.net.ConnectException
 class UserService(
     private val client: PocketBase
 ) {
+    fun creatUserViaEmail(email: String?, password: String?, passwordConfirm: String?){
+        if(email.isNullOrEmpty()){
+            throw IllegalArgumentException("Email cannot be empty or null")
+        }
+
+        if (password.isNullOrEmpty()){
+            throw IllegalArgumentException("Password cannot be empty or null")
+        }
+
+        if (passwordConfirm.isNullOrEmpty()){
+            throw IllegalArgumentException("Password confirm cannot be empty or null")
+        }
+
+        val enrichedBody = emptyMap<String, Any>().toMutableMap()
+        enrichedBody["email"] = email
+        enrichedBody["password"] = password
+        enrichedBody["passwordConfirm"] = passwordConfirm
+
+        val enrichedHeaders = emptyMap<String, String>().toMutableMap()
+        enrichedHeaders["Content-Type"] = "application/json"
+
+        val objectMapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+        val requestBody: String = objectMapper.writeValueAsString(enrichedBody)
+
+        val httpClient = HttpClient.newBuilder().build()
+
+        val requestBuilder = HttpRequest.newBuilder()
+            .uri(URI.create("${client.baseUrl}/api/users"))
+
+        enrichedHeaders.forEach{(header, value) ->
+            requestBuilder.header(header, value)
+        }
+
+        val request = requestBuilder
+            .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+            .build()
+
+        try{
+            val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+            println(response.statusCode())
+            if(response.statusCode() > 200){
+                throw Exception("Something went wrong while trying to create a new user via email")
+            }
+
+            val userObject = objectMapper.readValue(response.body(), UserModel::class.java)
+            UserAuth(userObject.token, userObject, emptyMap())
+        }catch (e: ConnectException){
+            println("Was not able to connect to the PocketBase server")
+        }catch (e: Exception){
+            println(e.message)
+        }
+    }
+
     fun listAuthMethods(): Map<Any, Any>? {
         val httpClient = HttpClient.newBuilder().build()
 
